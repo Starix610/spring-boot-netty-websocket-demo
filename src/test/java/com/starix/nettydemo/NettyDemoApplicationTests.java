fadf45760e11282c9111a2f3100ad1bfb0bbbed2
shiwenjie <shiwenjie03@meituan.com>
2020-06-11 17:38:49 +0800

    Initial commit

---
+++ src/test/java/com/starix/nettydemo/NettyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starix.nettydemo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
